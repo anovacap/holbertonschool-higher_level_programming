966bc2aa084b051cc58c64addd77b716929bfb14
anovacap <anovacap@yahoo.com>
2018-09-12 01:16:48 +0000

    remove hard code number

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -4,7 +4,7 @@ number = random.randint(-10000, 10000)
 if number < 0:
     modnum = number % -10
     if modnum == 0:
-        print("Last digit of {} is 0 and is 0".format(number))
+        print("Last digit of {} is {} and is 0".format(number, modnum))
     else:
         print("Last digit of {} is {} and is less than 6 and not 0"
               .format(number, modnum))
@@ -17,4 +17,4 @@ if number > 0:
         print("Last digit of {} is {} and is less than 6 and not 0"
               .format(number, modnum))
     else:
-        print("Last digit of {} is 0 and is 0".format(number))
+        print("Last digit of {} is {} and is 0".format(number, modnum))
