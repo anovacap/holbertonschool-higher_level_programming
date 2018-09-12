9dcddad945f1f4d3262605f6832dd292a9f7468a
anovacap <anovacap@yahoo.com>
2018-09-12 01:18:32 +0000

    add 0 check

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -8,7 +8,7 @@ if number < 0:
     else:
         print("Last digit of {} is {} and is less than 6 and not 0"
               .format(number, modnum))
-if number > 0:
+if number >= 0:
     modnum = number % 10
     if modnum > 5:
         print("Last digit of {} is {} and is greater than 5"
