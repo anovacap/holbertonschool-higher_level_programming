4303366d8d231c5572d52da295396bd52f34bbd1
anovacap <anovacap@yahoo.com>
2018-09-13 20:11:53 +0000

    fix typo

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
     if len(sys.argv) - 1 == 0:
         print("{} arguments.".format(len(sys.argv) - 1))
     elif len(sys.argv) - 1 == 1:
-            print("{} argumtent:".format(len(sys.argv) - 1))
+            print("{} argument:".format(len(sys.argv) - 1))
             print("{}: {}".format(len(sys.argv) - 1, sys.argv[1]))
     else:
         print("{} arguments:".format(len(sys.argv) - 1))
