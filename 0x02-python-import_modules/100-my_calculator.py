fe9a7f4b5a8bc8adf72eec88898c8db9387e8c18
anovacap <anovacap@yahoo.com>
2018-09-14 03:05:43 +0000

    add int() to arg

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,22 +3,22 @@ if __name__ == "__main__":
     import sys
     import calculator_1
     arg = sys.argv
-    operators = {"+", "-", "*", "/"}
+    operators = {'+', '-', '*', '/'}
     if len(arg) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-    elif arg[2] not in operators:
+    elif arg[2] in operators:
+        if arg[2] == "+":
+            print("{} + {} = {}".format(arg[1], arg[3], calculator_1.add
+                                        (int(arg[1]), int(arg[3]))))
+        if arg[2] == "-":
+            print("{} - {} = {}".format(arg[1], arg[3], calculator_1.sub
+                                        (int(arg[1]), int(arg[3]))))
+        if arg[2] == "*":
+            print("{} * {} = {}".format(arg[1], arg[3], calculator_1.mul
+                                        (int(arg[1]), int(arg[3]))))
+        if arg[2] == "/":
+            print("{} / {} = {}".format(arg[1], arg[3], calculator_1.div
+                                        (int(arg[1]), int(arg[3]))))
+    else:
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
-    else:
-        if arg[2] == "+":
-            print("{} + {} = {}".format(arg[1], arg[3],
-                                        calculator_1.add(arg[1], arg[3])))
-        if arg[2] == "-":
-            print("{} - {} = {}".format(arg[1], arg[3],
-                                        calculator_1.sub(arg[1], arg[3])))
-        if arg[2] == "*":
-            print("{} * {} = {}".format(arg[1], arg[3],
-                                        calculator_1.mul(arg[1], arg[3])))
-        if arg[2] == "/":
-            print("{} / {} = {}".format(arg[1], arg[3],
-                                        calculator_1.div(arg[1], arg[3])))
