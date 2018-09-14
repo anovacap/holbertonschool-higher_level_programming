c7be69c38c79fd82d486502ba81d180ccdab7a1b
anovacap <anovacap@yahoo.com>
2018-09-14 00:36:15 +0000

    fix malloc

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -11,13 +11,11 @@ listint_t *insert_node(listint_t **head, int number)
 	listint_t *new_node;
 	listint_t *mover;
 
-	if (*head)
-	{
-		new_node = malloc(sizeof(listint_t));
-		if (new_node == NULL)
-			return (NULL);
-		new_node->n = number;
-	}
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->n = number;
+
 	if (*head == NULL)
 	{
 		*head = new_node;
