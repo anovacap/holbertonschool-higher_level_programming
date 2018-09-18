4a243352827b1ccf053077ce97965d87bcfd9626
anovacap <anovacap@yahoo.com>
2018-09-18 21:59:54 +0000

    remove counter

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    z = 0
     if matrix is None:
         return None
     for i in matrix:
         for x in i:
-            if z == i[len(i) - 1]:
+            if x == i[len(i) - 1]:
                 print("{:d}".format(x), end="")
             else:
                 print("{:d}".format(x), end=" ")
-            z += 1
         print()
