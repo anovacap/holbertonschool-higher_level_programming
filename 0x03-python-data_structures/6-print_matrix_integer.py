7980e90d83eb77106a02b6050d9726d8887ee467
anovacap <anovacap@yahoo.com>
2018-09-18 21:56:38 +0000

    prints a matrix of integers

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
+    z = 0
     if matrix is None:
         return None
     for i in matrix:
-        print(*i, sep=" ")
+        for x in i:
+            if z == i[len(i) - 1]:
+                print("{:d}".format(x), end="")
+            else:
+                print("{:d}".format(x), end=" ")
+            z += 1
+        print()
