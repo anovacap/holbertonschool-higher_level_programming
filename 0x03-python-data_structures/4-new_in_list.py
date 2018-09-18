ab6b6b0db0865744b2d009232186d749acad4d61
anovacap <anovacap@yahoo.com>
2018-09-18 17:28:55 +0000

    Initial commit ex.4

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    z = len(my_list)
+    nl = my_list[:]
+    if idx < 0 or idx > z:
+        return my_list
+    nl[idx] = element
+    return nl
