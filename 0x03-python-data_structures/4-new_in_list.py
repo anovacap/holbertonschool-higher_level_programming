c39a4761985e870502ffc42693047083ce33556c
anovacap <anovacap@yahoo.com>
2018-09-18 19:55:06 +0000

    add >= to if statement

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,7 +2,7 @@
 def new_in_list(my_list, idx, element):
     z = len(my_list)
     nl = my_list[:]
-    if idx < 0 or idx > z:
+    if idx < 0 or idx >= z:
         return my_list
     nl[idx] = element
     return nl
