67e3debd5b00c55ab58225ea0e257ed3c2923adf
anovacap <anovacap@yahoo.com>
2018-09-18 19:50:47 +0000

    add >= to if statement

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
     z = len(my_list)
-    if idx < 0 or idx > z:
+    if idx < 0 or idx >= z:
         return None
     if idx <= z:
         return my_list[idx]
