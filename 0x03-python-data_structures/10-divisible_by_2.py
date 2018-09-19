32a25605d1f366a534bee8da65bd00cd90c5d310
anovacap <anovacap@yahoo.com>
2018-09-19 02:07:34 +0000

    change var from a string to True/False

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -2,11 +2,11 @@
 def divisible_by_2(my_list=[]):
     if len(my_list) is 0:
         return None
-    x = "True"
-    y = "False"
+    x = True
+    y = False
     new_list = []
     for i in my_list:
-        if i % 2 == 0:
+        if i % 2 is 0:
             new_list.append(x)
         else:
             new_list.append(y)
