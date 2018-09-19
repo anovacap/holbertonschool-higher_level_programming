fb04ddb28e21d18eddc88fa9634ee025a936f464
anovacap <anovacap@yahoo.com>
2018-09-19 00:40:05 +0000

    change check for 0 length input

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     x = len(sentence)
-    if sentence is None:
+    if len(sentence) is 0:
         return x, None
     else:
         j = sentence[0]
