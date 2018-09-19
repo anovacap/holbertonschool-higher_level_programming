818c01c371f746077f15d105b8da589754ea50c8
anovacap <anovacap@yahoo.com>
2018-09-19 02:30:32 +0000

    change None check

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
     l = len(my_list)
-    if l is 0:
+    if l == 0:
         return None
-    if idx < 0 or idx >= l:
+    if idx <= 0 or idx >= l:
         return my_list
     else:
         del my_list[idx]
