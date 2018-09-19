2ccbe1e12f8da0ab4b158ad699eb6cf54c73f51a
anovacap <anovacap@yahoo.com>
2018-09-19 01:15:10 +0000

    check first element of the list

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,7 +2,7 @@
 def max_integer(my_list=[]):
     if my_list is None:
         return None
-    num = min(my_list)
+    num = my_list[0]
     for i in my_list:
         if i > num:
             num = i
