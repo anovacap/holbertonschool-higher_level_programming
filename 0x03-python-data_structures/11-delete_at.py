80e9b85f5958dbeae4359298e504535487ecb764
anovacap <anovacap@yahoo.com>
2018-09-19 02:36:38 +0000

    change logic to test for True

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
     l = len(my_list)
-    if l == 0:
-        return None
-    if idx < 0 or idx >= l:
-        return my_list
-    else:
+    if idx >= 0 or idx < l:
         del my_list[idx]
-        return my_list
+    return my_list
