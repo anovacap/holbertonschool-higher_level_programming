ad2dff383169ebd983e0617e29fa9639013ec231
anovacap <anovacap@yahoo.com>
2018-09-20 23:51:12 +0000

    test values in dict for max

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary != None:
-        j = max(a_dictionary.keys())
-        return j
+    if a_dictionary is not None:
+        j = max(a_dictionary.values())
+        if j in a_dictionary.values():
+            p = {k for k, v in a_dictionary.items() if v == j}
+            s = p.pop()
+            return s
     else:
         return None
