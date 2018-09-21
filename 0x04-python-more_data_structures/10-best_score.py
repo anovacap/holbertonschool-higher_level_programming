7f11c401f35a7152138d117d9314be0402c83e92
anovacap <anovacap@yahoo.com>
2018-09-21 00:39:23 +0000

    test for None

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is not None:
+    if a_dictionary is None or a_dictionary == {}:
+        return None
+    else:
         j = max(a_dictionary.values())
         if j in a_dictionary.values():
             p = {k for k, v in a_dictionary.items() if v == j}
             s = p.pop()
             return s
-    else:
-        return None
