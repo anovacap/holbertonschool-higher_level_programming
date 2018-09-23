29b3729373ae5050aaa33ae384f9325fb8441018
anovacap <anovacap@yahoo.com>
2018-09-23 17:53:59 +0000

    improve test for empty list

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    if my_list is None or my_list = []:
+    if my_list is None or my_list == []:
         return 0
     mult_tup = [x * y for x, y in my_list]
     sum_tup = sum(mult_tup)
