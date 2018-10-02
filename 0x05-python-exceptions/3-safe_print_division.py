4a3598c4267a7b8ae56089cef12496fb05e66dc6
anovacap <anovacap@yahoo.com>
2018-10-02 00:36:20 +0000

    Add finally: and ZeroDivisionError exception

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
     try:
-        c = a / b
-        print("Inside result: {:.1f}".format(c))
-        return c
-    except(ZeroDivisionError):
-        pass
+        result = a / b
+    except ZeroDivisionError:
+        result = "None"
+    finally:
+        print("Inside result: {}".format(result))
+        return result
