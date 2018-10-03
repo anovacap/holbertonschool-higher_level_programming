b9c81e6530104ebee10077ac9c7edd30fafde6b0
anovacap <anovacap@yahoo.com>
2018-10-03 03:58:39 +0000

    add test for len(value)

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -17,7 +17,7 @@ class Square:
 
     @position.setter
     def position(self, value):
-        if type(value) is not tuple or value < 0:
+        if type(value) is not tuple or value < 0 or len(value) is not 2:
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
