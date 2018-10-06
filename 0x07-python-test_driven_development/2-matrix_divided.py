b76123b4a06df16a9cddfc36e56f51a3e02642ae
anovacap <anovacap@yahoo.com>
2018-10-06 04:02:08 +0000

    test for empty matrix list

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -5,19 +5,28 @@ Defines function that divides all elements of a matrix
 
 
 def matrix_divided(matrix, div):
+    """
+    Divides an even matrix by an integer
+    """
     m = []
+    if matrix is None:
+        raise TypeError(
+            "matrix must be a matrix (list of lists) of integers/floats")
     it = iter(matrix)
     len_mat = len(next(it))
     if not all(len(k) == len_mat for k in it):
         raise TypeError("Each row of the matrix must have the same size")
+    if matrix == [] or matrix == [[]]:
+        raise TypeError(
+            "matrix must be a matrix (list of lists) of integers/floats")
     for x in matrix:
         if not isinstance(x, list):
-            raise TypeError("matrix must be a matrix (list of lists)\
-            of integers/floats")
+            raise TypeError(
+                "matrix must be a matrix (list of lists) of integers/floats")
         for y in x:
             if type(y) is not int and type(y) is not float:
-                raise TypeError("matrix must be a matrix (list of lists)\
-                of integers/floats")
+                raise TypeError("matrix must be a matrix"
+                                " (list of lists) of integers/floats")
         if div == 0:
             raise ZeroDivisionError("division by zero")
         if isinstance(div, int):
