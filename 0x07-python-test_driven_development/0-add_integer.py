6bbe998f95cb4beaaea99b72e6105492a41c95c7
anovacap <anovacap@yahoo.com>
2018-10-06 19:55:27 +0000

    conformed to pep8 format

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -13,12 +13,12 @@ def add_integer(a, b=98):
         raise TypeError("a must be an integer")
     if b is float('inf') or b is float('nan'):
         raise TypeError("b must be an integer")
-    if type(a) is  int or type(a) is  float:
+    if type(a) is int or type(a) is float:
         a = int(a)
     else:
         raise TypeError("a must be an integer")
-    if type(b) is  int or type(b) is  float:
-        b =int(b)
+    if type(b) is int or type(b) is float:
+        b = int(b)
     else:
         raise TypeError("b must be an integer")
     c = a + b
