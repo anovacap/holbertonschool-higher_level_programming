bdaedfd8c0ee0596d9ee4393ed3168ee19882a59
anovacap <anovacap@yahoo.com>
2018-10-06 19:52:07 +0000

    tests for nan and inf float

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -8,12 +8,17 @@ def add_integer(a, b=98):
     """
     Adds 2 intgers or floats together
     """
-    if type(a) is int or type(a) is float:
+
+    if a is float('inf') or a is float('nan'):
+        raise TypeError("a must be an integer")
+    if b is float('inf') or b is float('nan'):
+        raise TypeError("b must be an integer")
+    if type(a) is  int or type(a) is  float:
         a = int(a)
     else:
         raise TypeError("a must be an integer")
-    if type(b) is int or type(b) is float:
-        b = int(b)
+    if type(b) is  int or type(b) is  float:
+        b =int(b)
     else:
         raise TypeError("b must be an integer")
     c = a + b
