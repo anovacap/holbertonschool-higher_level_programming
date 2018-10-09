2fa5cec28ceb2773455dd7670d36a3d97eff1c65
anovacap <anovacap@yahoo.com>
2018-10-09 07:41:20 +0000

    test for 0 in __str__

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -69,8 +69,10 @@ class Rectangle:
         __str__
         returns: printed rectangle
         """
+        if self.width is 0 or self.height is 0:
+            return " "
         s = ""
-        for i in range(self.height -1):
+        for i in range(self.height - 1):
             s += "#" * self.width + "\n"
             if i is self.height - 2:
                 s += "#" * self.width
