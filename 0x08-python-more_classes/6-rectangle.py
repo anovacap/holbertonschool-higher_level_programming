4173daa5327cdd1ebbd5945d0873040b8da680de
anovacap <anovacap@yahoo.com>
2018-10-09 04:11:15 +0000

    Remove tests in inti

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -14,14 +14,6 @@ class Rectangle:
     def __init__(self, width=0, height=0):
         self.__width = width
         self.__height = height
-        if type(width) is not int:
-            raise TypeError("width must be an integer")
-        if width < 0:
-            raise ValueError("width must be >= 0")
-        if type(height) is not int:
-            raise TypeError("height must be an integer")
-        if height < 0:
-            raise ValueError("height must be >= 0")
         Rectangle.number_of_instances += 1
     """
     Defines function width to retrieve __width
