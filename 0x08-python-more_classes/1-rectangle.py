2fda8a2dc9c67dfd2e5217ab7b5a7228d6add710
anovacap <anovacap@yahoo.com>
2018-10-09 16:14:11 +0000

    updated var in __init__

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -11,8 +11,8 @@ class Rectangle:
         __init__ method
         Args: width (int), height (int)
         """
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
@@ -24,9 +24,9 @@ class Rectangle:
         width
         Args: value (int)
         """
-        if type(self.__width) is not int:
+        if type(value) is not int:
             raise TypeError("width must be an integer")
-        if self.__width < 0:
+        if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
 
@@ -40,8 +40,8 @@ class Rectangle:
         height
         Args: value (int)
         """
-        if type(self.__height) is not int:
+        if type(value) is not int:
             raise TypeError("height must be an integer")
-        if self.__height < 0:
+        if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
