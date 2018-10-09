231672e01b19c1d2986f5fa9bb8f785b6c6a3a8f
anovacap <anovacap@yahoo.com>
2018-10-09 05:06:52 +0000

    Change commenting style

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -5,56 +5,60 @@ Defines Class Rectangle
 
 
 class Rectangle:
-    __width = None
-    __height = None
-    """
-    function __init__ initializes class Rectangle
-    """
+
     def __init__(self, width=0, height=0):
+        """
+        __init__ method
+        Args: width (int), height (int)
+        """
         self.__width = width
         self.__height = height
-    """
-    Defines function width to retrieve __width
-    """
+
     @property
     def width(self):
         return self.__width
-    """
-    funtction width setter
-    """
+
     @width.setter
     def width(self, value):
+        """
+        width
+        Args: value (int)
+        """
         if type(value) is not int:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
-    """
-    function height getter
-    """
+
     @property
     def height(self):
         return self.__height
-    """
-    function height setter
-    """
+
     @height.setter
     def height(self, value):
+        """
+        height
+        Args: value (int)
+        """
         if type(value) is not int:
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
-    """
-    function area returns rectangle area
-    """
+
     def area(self):
+        """
+        area
+        retruns: self.width * self.height
+        """
         area = self.width * self.height
         return area
-    """
-    function perimeter returns rectangle perimeter
-    """
+
     def perimeter(self):
+        """
+        perimeter
+        returns: (self.width + self.height) * 2
+        """
         if self.width is 0 or self.height is 0:
             return 0
         perimeter = (self.width + self.height) * 2
