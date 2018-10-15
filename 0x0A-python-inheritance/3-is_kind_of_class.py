213ef95c7cdf7570a72a6e485b327424a79efd9e
anovacap <anovacap@yahoo.com>
2018-10-15 22:22:14 +0000

    ex.3

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Defines is_kind_of_class
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """
+    Function is_kind_of_class - args - obj, a_class
+    """
+    return isinstance(obj, a_class)
