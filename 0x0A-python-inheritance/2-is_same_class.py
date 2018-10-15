ae35cb431b609de31ef566eca64486716e46b733
anovacap <anovacap@yahoo.com>
2018-10-15 22:18:18 +0000

    Ex.2

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Defines is function is_same_class
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+    function is_same_class() args - obj, a_class
+    """
+    return type(obj) is a_class
