543792a7ac198dd6cb992ce2e0300505a91ed8a3
anovacap <anovacap@yahoo.com>
2018-10-15 22:56:44 +0000

    ex.6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Defines class BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """
+    Class BaseGeometry
+    """
+    def area(self):
+        """
+        Function area - no args
+        """
+        raise Exception("area() is not implemented")
