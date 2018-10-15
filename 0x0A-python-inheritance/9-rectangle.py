668fa4058343280de08ad26dc10351ef477141b3
anovacap <anovacap@yahoo.com>
2018-10-15 23:59:07 +0000

    ex.9

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+"""
+Defines BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """
+    Class BaseGeometry
+    """
+    def area(self):
+        """
+        Function area - no args
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """
+        Func integer_validator - validates value - args - name, value
+        """
+        name = name
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
+    """
+    Class Rectangle
+    """
+    def __init__(self, width, height):
+        """
+        func - __init__ - args - width, height
+        """
+        self.__width = width
+        self.__height = height
+        self.integer_validator(self.__width, self.__height)
+
+    def area(self):
+        """
+        func area - no arguments
+        """
+        return self.__width * self.__height
+
+    def __str__(self):
+        """
+        func str - no args
+        """
+        return str("[Rectangle] {}/{}".format(self.__width, self.__height))
