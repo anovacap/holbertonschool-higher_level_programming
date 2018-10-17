1f0d09121cd172d82d9af345a01c6ea5285e3ae6
anovacap <anovacap@yahoo.com>
2018-10-17 03:59:39 +0000

    change counter

--- 0x0B-python-input_output/4-append_write.py
+++ 0x0B-python-input_output/4-append_write.py
@@ -8,8 +8,6 @@ def append_write(filename="", text=""):
     z = 0
     with open(filename, mode='a', encoding='utf-8') as a_file:
         a_file.write(text)
-    with open(filename, encoding='utf-8') as a_file:
-        for a_line in a_file:
-            for i in a_line:
-                z += 1
+    for i in text:
+        z += 1
     return z
