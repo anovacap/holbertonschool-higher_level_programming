a9787ef8d714c95de0a8336529307ba5305c970b
anovacap <anovacap@yahoo.com>
2018-10-17 00:12:53 +0000

    ex.11

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+
+class Student:
+    """
+    Class Student
+    """
+    def __init__(self, first_name, last_name, age):
+        """
+        func __init__ args - first_name, last_name, age
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """
+        func to_json no args
+        """
+        return self.__dict__
