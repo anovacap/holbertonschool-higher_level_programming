c8a4478747e9c74b6de1f81e65327fcfd21a555f
anovacap <anovacap@yahoo.com>
2018-12-04 04:30:57 +0000

    utf8mb4

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,2 +1,2 @@
 -- converts hbtn_0c_0 database to UTF8
-ALTER TABLE first_table MODIFY `name` VARCHAR(256) CHARACTER SET utf8;
+ALTER TABLE first_table MODIFY `name` VARCHAR(256) CHARACTER SET utf8mb4;
