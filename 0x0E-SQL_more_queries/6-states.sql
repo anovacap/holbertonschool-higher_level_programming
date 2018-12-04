a9751378d65cca62bfd0cc33ae9f234d7a1763d5
Damon Nyhan <damon.nyhan@gmail.com>
2018-12-04 14:20:17 -0800

    unique

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,6 +1,5 @@
 -- creates the database hbtn_0d_usa and the table states
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS states(
-    id INT NOT NULL AUTO_INCREMENT PRIMARY kEY,
+    id INT NOT NULL AUTO_INCREMENT UNIQUE PRIMARY kEY,
     name VARCHAR(256));
-    
