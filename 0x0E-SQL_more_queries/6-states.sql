d3b1065e5beccfc2fe8a59898cb23a6f3af3253a
Damon Nyhan <damon.nyhan@gmail.com>
2018-12-04 14:22:44 -0800

    remove unique add not null

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,5 +1,5 @@
 -- creates the database hbtn_0d_usa and the table states
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS states(
-    id INT NOT NULL AUTO_INCREMENT UNIQUE PRIMARY kEY,
-    name VARCHAR(256));
+    id INT NOT NULL AUTO_INCREMENT PRIMARY kEY,
+    name VARCHAR(256) NOT NULL);
