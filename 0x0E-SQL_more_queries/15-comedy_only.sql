3f5b611927107d8f1b7b008a32899e81994eab24
Damon Nyhan <damon.nyhan@gmail.com>
2018-12-07 07:42:26 -0800

    long form

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,8 +1,8 @@
 -- lists all Comedy shows in the database hbtn_0d_tvshows
-SELECT a.title FROM tv_shows a
-    JOIN tv_show_genres b
-    ON a.id = b.show_id
-    JOIN tv_genres c
-    ON b.genre_id = c.id
-    WHERE c.name = "Comedy"
-    ORDER BY a.title;
+SELECT tv_shows.title FROM tv_shows
+    JOIN tv_show_genres
+    ON tv_shows.id = tv_show_genres.show_id
+    JOIN tv_genres
+    ON tv_show_genres.genre_id = tv_genres.id
+    WHERE tv_genres.name = "Comedy"
+    ORDER BY tv_shows.title;
