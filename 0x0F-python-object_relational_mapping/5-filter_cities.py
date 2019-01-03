b606d26c8b35e6ee76e6003c9e560b93a535e8d9
anovacap <anovacap@yahoo.com>
2019-01-03 19:38:53 +0000

    line length reduced

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -17,7 +17,8 @@ def main(argv):
                          passwd=argv[2],
                          db=argv[3])
     cur = db.cursor()
-    cur.execute("SELECT cities.name FROM cities JOIN states ON cities.state_id = states.id WHERE states.name=%s", (argv[4],))
+    cur.execute("SELECT cities.name FROM cities JOIN states ON cities."
+                "state_id = states.id WHERE states.name=%s", (argv[4],))
 
     print(", ".join([row[0] for row in cur]))
     db.close()
