6e8a76f1de9310a33cf6fe5f13ce4d8313755483
anovacap <anovacap@yahoo.com>
2019-01-03 08:28:36 +0000

    add ex.5 ex.6

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Ltakes in the name of a state as an argument and lists all
+ cities of that state,"""
+import MySQLdb
+import sys
+
+
+def main(argv):
+    """func - main - argv"""
+    if len(argv) != 5:
+        print("Enter 4 arguments")
+        return
+
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT cities.name FROM cities JOIN states ON cities.state_id = states.id WHERE states.name=%s", (argv[4],))
+
+    print(", ".join([row[0] for row in cur]))
+    db.close()
+
+if __name__ == "__main__":
+    import sys
+    main(sys.argv)
---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Defines class State sets up states database"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+Base = declarative_base()
+
+
+class State(Base):
+    """State inherits from Base"""
+    __tablename__ = 'states'
+    id = Column(Integer, unique=True, nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)
