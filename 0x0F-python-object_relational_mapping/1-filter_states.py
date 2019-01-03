847ee9c8148acba0c16d3efad7dcca533d45eb4d
anovacap <anovacap@yahoo.com>
2019-01-03 08:25:56 +0000

    add ex.1

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""lists all states with a name starting with N"""
+import MySQLdb
+import sys
+
+
+def main(argv):
+    """func - main - args"""
+    if len(argv) != 4:
+        print("Enter 3 arguments")
+        return
+
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%'")
+
+    for row in cur.fetchall():
+        print(row)
+    db.close()
+
+if __name__ == "__main__":
+    import sys
+    main(sys.argv)
