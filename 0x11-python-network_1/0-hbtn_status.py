167e7d11ab9b8ad0d556ad9ca5e2d3ff925dd5a2
anovacap <anovacap@yahoo.com>
2019-01-21 16:20:39 +0000

    now not executable on import

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
 """script that fetches https://intranet.hbtn.io/status"""
 import urllib.request
-with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-    html = response.read()
-print("Body response:\n\t- type: {}\n\t- content: {}\n\t- utf8"
-      " content: {}".format(type(html), html, html.decode("utf-8")))
+
+
+def main():
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        html = response.read()
+    print("Body response:\n\t- type: {}\n\t- content: {}\n\t- utf8"
+          " content: {}".format(type(html), html, html.decode("utf-8")))
+
+if __name__ == "__main__":
+    main()
