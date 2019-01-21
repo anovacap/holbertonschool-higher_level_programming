9d42c999687ba6b6bf7670bcb12f22adbdeb521c
anovacap <anovacap@yahoo.com>
2019-01-21 21:43:03 +0000

    use X-Request-Id not x-request-id

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -9,7 +9,7 @@ import sys
 def main():
     r = requests.get(sys.argv[1])
     heder = r.headers
-    print(heder["x-request-id"])
+    print(heder["X-Request-Id"])
 
 if __name__ == "__main__":
     main()
