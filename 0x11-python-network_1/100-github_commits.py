38da137a6b480a29993f7878932cdd03f44ce6ff
anovacap <anovacap@yahoo.com>
2019-01-22 04:11:32 +0000

    add ex.100

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Python script that takes your Github repo and user)
+ and uses the Github API to display commit name and sha number"""
+import requests
+import sys
+
+
+def main():
+    u = sys.argv[2]
+    r = sys.argv[1]
+    try:
+        f_url = "https://api.github.com/repos/" + u + "/" + r + "/commits"
+        req = requests.get(f_url)
+        js = req.json()
+        for d in js:
+            print("{}: {}".format(d['sha'], d['commit']['author']['name']))
+    except TypeError and KeyError:
+        print("None")
+
+if __name__ == "__main__":
+    main()
