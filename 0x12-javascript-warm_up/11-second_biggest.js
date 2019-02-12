f62a3b203b06abcbd418cf541911a02fd894ab9e
anovacap <anovacap@yahoo.com>
2019-02-12 23:26:56 +0000

    add ex.11

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+if (process.argv.length <= 3) {
+  console.log(0);
+} else {
+  let ar = process.argv.slice(2);
+  let newAr = ar.sort();
+  newAr.reverse();
+  console.log(newAr[1]);
+}
