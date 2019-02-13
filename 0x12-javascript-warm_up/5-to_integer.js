23985441a05449c0908473d42fd093bcff65e973
anovacap <anovacap@yahoo.com>
2019-02-13 00:16:00 +0000

    parseInt float

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 
 const myVar = process.argv[2];
-if (!myVar || parseInt(myVar) === isNaN) {
+if (!myVar || isNaN(myVar)) {
   console.log('Not a number');
 } else if (parseInt(myVar) === Number) {
   console.log('My number: %d', parseInt(myVar, 10));
 } else {
-  console.log('My number: %d', Math.floor(myVar));
+  console.log('My number: %d', Math.floor(parseInt(myVar, 10)));
 }
