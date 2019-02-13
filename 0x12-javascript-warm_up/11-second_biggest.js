2f8708eb4ded88705449b65b4a0feb538d76c950
anovacap <anovacap@yahoo.com>
2019-02-13 05:24:59 +0000

    new sort)

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,7 +4,7 @@ if (process.argv.length <= 3) {
   console.log(0);
 } else {
   let ar = process.argv.slice(2);
-  let newAr = ar.sort();
+  let newAr = ar.sort((a, b) => a - b);
   if (Math.sign(newAr[1]) === -1) {
     console.log(parseInt(newAr[1], 10));
   } else {
